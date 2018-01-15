ef2618cf249b7ed67be0a548040c86a93287dc5d
it1513 <31645578+it1513@users.noreply.github.com>
2018-01-15 10:02:08 +0100

    Add files via upload

---
+++ pio_led/src/main.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+
+class Led {
+    public:
+        Led(int);
+        void On();
+        void Off();
+    private:
+        int outPin;
+        bool on;    
+};
+
+Led::Led(int outpin){
+    this->outPin = outpin;
+    this->on = false;
+}
+void Led::On(){
+    digitalWrite(this->outPin, HIGH);
+    this->on = true;
+}
+
+void Led::Off(){
+    digitalWrite(this->outPin, LOW);
+    this->on = false;
+}
+
+Led ledky[3]{16,5,4,};
+
+void setup() {
+    // put your setup code here, to run once:
+}
+
+void loop() {
+    for(int i = 0;i < 7;i++){
+        ledky[i].On();
+        delay(50);
+        ledky[i].Off();
+        delay(50);
+    }
+}
