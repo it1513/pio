707b2e9ec51055a8478fb237319a6c4c190e31d2
it1513 <31645578+it1513@users.noreply.github.com>
2018-01-22 10:04:24 +0100

    Add files via upload

--- pio_led/src/main.cpp
+++ pio_led/src/main.cpp
@@ -5,6 +5,7 @@ class Led {
         Led(int);
         void On();
         void Off();
+        int getPin();
     private:
         int outPin;
         bool on;    
@@ -18,23 +19,73 @@ void Led::On(){
     digitalWrite(this->outPin, HIGH);
     this->on = true;
 }
+int Led::getPin(){
+    return this->outPin;
+}
 
 void Led::Off(){
     digitalWrite(this->outPin, LOW);
     this->on = false;
 }
 
-Led ledky[3]{16,5,4};
+Led ledky[7]{13,12,14,0,4,5,16};
+int interruptPin = 2;
+bool state =false;
 
-void setup() {
-    // put your setup code here, to run once:
+void blink(){
+     
+     state = !state;
 }
 
-void loop() {
-    for(int i = 0;i < 3;i++){
-        ledky[i].On();
-        delay(50);
-        ledky[i].Off();
-        delay(50);
+void setup(){
+    for(int i = 0;i < 7;i++){
+       pinMode(ledky[i].getPin(),OUTPUT);
     }
+    pinMode(interruptPin, LOW);
+    attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);;
+
+}
+
+void cislo(int i){
+    if(i == 1 || i == 3 || i == 5){
+        ledky[3].On(); // 3
+    }
+    if(i == 2 || i == 3 || i == 4 || i == 5 || i == 6){
+        ledky[2].On(); // 13
+        ledky[6].On(); // 5
+    }
+    if(i == 4 || i == 5 || i == 6){
+        ledky[0].On(); // 14
+        ledky[4].On(); // 0
+    }
+    if(i == 6){
+        ledky[1].On(); // 12
+        ledky[5].On(); // 4
+    }
+}
+
+void offAll(){
+    for(int i=0 ; i < 7;i++){
+        ledky[i].Off();    
+    }
+}
+
+void randNumb(int del){ 
+    offAll();
+    cislo( (rand() % 6 + 1) );
+    delay(del);
+}
+void loop(){
+    
+    randNumb(100);
+
+   if(state){
+       int del = 0; 
+        while(del < 1000){
+            randNumb(del);
+            del += 100;
+        }
+        randNumb(5000);
+        state = false;
+     }
 }
